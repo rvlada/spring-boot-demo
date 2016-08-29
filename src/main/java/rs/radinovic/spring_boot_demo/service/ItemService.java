078716c440aa7e1cc7ab1b5f23f272983709437e
Vladimir Radinovic <vladimir@radinovic.rs>
2016-08-29 16:58:36 +0200

    Add ItemService

---
+++ src/main/java/rs/radinovic/spring_boot_demo/service/ItemService.java
@@ -0,0 +1,39 @@
+package rs.radinovic.spring_boot_demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.radinovic.spring_boot_demo.domain.Item;
+import rs.radinovic.spring_boot_demo.domain.ItemRepository;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public long saveItem(Item item) {
+        return itemRepository.save(item).getId();
+    }
+
+    public Item findItem(long id) {
+        return itemRepository.findOne(id);
+    }
+
+    public List<Item> findAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public long updateItem(Item item) {
+        return itemRepository.save(item).getId();
+    }
+
+    public void deleteItem(Item item) {
+        itemRepository.delete(item);
+    }
+}
