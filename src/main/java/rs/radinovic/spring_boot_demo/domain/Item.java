8d7ae5244b2002105f3f4601db518bd93c45c1de
Vladimir Radinovic <vladimir@radinovic.rs>
2016-08-29 16:03:42 +0200

    Add Item entity

---
+++ src/main/java/rs/radinovic/spring_boot_demo/domain/Item.java
@@ -0,0 +1,45 @@
+package rs.radinovic.spring_boot_demo.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty
+    private String name;
+
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
