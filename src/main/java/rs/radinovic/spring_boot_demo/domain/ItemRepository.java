c68f596d4ae08edf41ffec117fb2626c94cbf04a
Vladimir Radinovic <vladimir@radinovic.rs>
2016-08-29 16:21:53 +0200

    Add ItemRepository

---
+++ src/main/java/rs/radinovic/spring_boot_demo/domain/ItemRepository.java
@@ -0,0 +1,8 @@
+package rs.radinovic.spring_boot_demo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
