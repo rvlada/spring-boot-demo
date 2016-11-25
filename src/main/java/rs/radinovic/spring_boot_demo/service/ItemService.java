21bdf9908fcabacfb4c7192c2c17a6f589d52375
Vladimir Radinovic <vladimir@radinovic.rs>
2016-11-25 11:19:13 +0100

    Add ItemService interface

--- src/main/java/rs/radinovic/spring_boot_demo/service/ItemService.java
+++ src/main/java/rs/radinovic/spring_boot_demo/service/ItemService.java
@@ -1,44 +1,18 @@
 package rs.radinovic.spring_boot_demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import rs.radinovic.spring_boot_demo.domain.Item;
-import rs.radinovic.spring_boot_demo.domain.ItemRepository;
 
 import java.util.List;
 
-@Service
-public class ItemService {
+public interface ItemService {
 
-    private final ItemRepository itemRepository;
+    long saveItem(Item item);
 
-    @Autowired
-    public ItemService(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
+    Item findItem(long id);
 
-    public long saveItem(Item item) {
-        return itemRepository.save(item).getId();
-    }
+    List<Item> findAllItems();
 
-    public Item findItem(long id) {
-        return itemRepository.findOne(id);
-    }
+    Item updateItem(Item item);
 
-    public List<Item> findAllItems() {
-        return itemRepository.findAll();
-    }
-
-    public Item updateItem(Item item) {
-        Item oldItem = findItem(item.getId());
-        if (oldItem == null) {
-            throw new IllegalArgumentException("Error, trying to update non-existing item.");
-        }
-        return itemRepository.save(item);
-    }
-
-    public void deleteItem(long id) {
-        Item item = findItem(id);
-        itemRepository.delete(item);
-    }
+    void deleteItem(long id);
 }
